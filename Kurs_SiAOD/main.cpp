471890c2585a394a94ec9b86410cc3d2bdfbb227
akseart <akseart@ya.ru>
2018-02-27 16:48:45 +0300

    Initial Commit

---
+++ Kurs_SiAOD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Kurs_SiAOD
+//
+//  Created by Артем on 27.02.2018.
+//  Copyright © 2018 Артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
