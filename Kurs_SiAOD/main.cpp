47333e9cfda7913b32c13af12ae209aebc876e99
akseart <akseart@ya.ru>
2018-02-28 22:47:38 +0300

    v 0.1

--- Kurs_SiAOD/main.cpp
+++ Kurs_SiAOD/main.cpp
@@ -7,9 +7,54 @@
 //
 
 #include <iostream>
+using namespace std;
+enum color{RED,BLACK};
+class Vertex {
+private:
+    color color_;
+    int value_;
+    Vertex *left_, *right_, *parent_;
+public:
+    Vertex(int,Vertex *);
+    int getValue();
+    color getColor();
+    
+    Vertex getLeft();
+    Vertex getRight();
+    Vertex getParent();
+    
+    void putValue(int);
+    void putColor(bool);
+    
+    void putLeft(Vertex);
+    void putRight(Vertex);
+    void putParent(Vertex);
+    
+};
+Vertex::Vertex(int value,Vertex *parent){
+    color_  = RED; //Устанавливаем красный цвет
+    value_  = value;
+    parent_ = parent;
+};
 
+class RedBlackTree {
+private:
+    Vertex *roots_;
+public:
+    RedBlackTree() {
+        roots_=NULL;
+    };
+    void addNewVertex(int);
+    void checkingRedBlackProperties();
+    void deleteVertex(int);
+};
+void RedBlackTree::addNewVertex(int value){
+    Vertex *parent = this->roots_;
+    Vertex *newVertex = new Vertex (value, NULL);
+};
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    //std::cout << "Hello, World!\n";
     return 0;
-}
+};
+
